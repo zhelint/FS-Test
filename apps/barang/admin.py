cfe242100c0e20f0d3c3e4c553669ad8cafacbee
Amatul Sharin <sharinamatul@gmail.com>
2024-06-14 00:56:43 +0700

    register models to admin panel

--- apps/barang/admin.py
+++ apps/barang/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import KategoriBarang, Barang, SatuanBarang
 
 # Register your models here.
+admin.site.register(KategoriBarang)
+admin.site.register(Barang)
+admin.site.register(SatuanBarang)
