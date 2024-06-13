eab387d7bb2255e70c4129cacbda86dbc9c340f5
Amatul Sharin <sharinamatul@gmail.com>
2024-06-14 01:03:15 +0700

    add url to dashboard

---
+++ apps/home/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+]
