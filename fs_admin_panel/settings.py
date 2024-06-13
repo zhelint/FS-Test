be6dff2595320c2b07dd2656cc9d0e4bd8e9cdf2
Amatul Sharin <sharinamatul@gmail.com>
2024-06-14 01:01:28 +0700

    add django htmx middleware

--- fs_admin_panel/settings.py
+++ fs_admin_panel/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'fs_admin_panel.urls'
